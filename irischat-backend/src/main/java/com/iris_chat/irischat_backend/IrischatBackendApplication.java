17fcce98133a25efa81ab24a93a1d8e9a5ac3892
dtHieu076 <dangthanhhieudth03@gmail.com>
2026-04-06 00:19:00 +0700

    Tạo project

---
+++ irischat-backend/src/main/java/com/iris_chat/irischat_backend/IrischatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iris_chat.irischat_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrischatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrischatBackendApplication.class, args);
+	}
+
+}
---
+++ irischat-backend/src/test/java/com/iris_chat/irischat_backend/IrischatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iris_chat.irischat_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrischatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
