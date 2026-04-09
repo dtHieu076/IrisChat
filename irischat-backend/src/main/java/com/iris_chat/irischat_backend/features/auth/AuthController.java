bbcbc192e2254e23754344bcd3d0a93ec033d516
dtHieu076 <dangthanhhieudth03@gmail.com>
2026-04-09 21:09:45 +0700

    Bổ sung login

--- irischat-backend/src/main/java/com/iris_chat/irischat_backend/features/auth/AuthController.java
+++ irischat-backend/src/main/java/com/iris_chat/irischat_backend/features/auth/AuthController.java
@@ -3,9 +3,25 @@ package com.iris_chat.irischat_backend.features.auth;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.iris_chat.irischat_backend.share.dto.auth.request.LoginRequest;
+import com.iris_chat.irischat_backend.share.dto.auth.response.LoginResponse;
+import com.iris_chat.irischat_backend.share.service.AuthService;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+    
+    @PostMapping("/login")
+    public LoginResponse login(LoginRequest request) {
 
+        LoginResponse response = authService.login(request);
+        return response;
+    }
     
 }
--- irischat-backend/src/main/java/com/iris_chat/irischat_backend/features/auth/AuthServiceImpl.java
+++ irischat-backend/src/main/java/com/iris_chat/irischat_backend/features/auth/AuthServiceImpl.java
@@ -1,10 +1,14 @@
 package com.iris_chat.irischat_backend.features.auth;
 
+import org.springframework.stereotype.Service;
+
 import com.iris_chat.irischat_backend.share.dto.auth.request.LoginRequest;
 import com.iris_chat.irischat_backend.share.dto.auth.response.LoginResponse;
 import com.iris_chat.irischat_backend.share.service.AuthService;
 
+@Service
 public class AuthServiceImpl implements AuthService {
+    
     @Override
     public LoginResponse login(LoginRequest request) {
         // Implementation for login logic
